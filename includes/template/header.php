26549f9e1b31c5e99f48c73e642d7cccf81d85e5
chillinhen <c.hillen@gmx.net>
2018-03-11 11:39:27 +0100

    ready for desktop

--- includes/template/header.php
+++ includes/template/header.php
@@ -1,13 +1,21 @@
 <header id="header">
-    <div id="top-bar">
-        <div class="d-flex justify-content-around flex-sm-column">
-<!--        <div class="row justify-content-around justify-content-lg-center flex-md-row flex-sm-column">-->
-            <a class="navbar-brand bg-image order-sm-2 order-lg-1" href="index.php"><span>familienbande24.de</span></a>
-            <div class="d-none d-xl-block order-sm-1 order-lg-2"><img class="leaderboard" src="https://dummyimage.com/970x90/eee/00ffd5.png." alt="Dummy Leaderboard"></div>      
-            <button class="navbar-toggler align-self-center d-block d-md-none" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <div id="top-bar" class="col-12 mt-4">
+        <div class="d-flex flex-lg-column flex-xs-row flex-xl-row justify-content-between align-items-center">
+            <!--        <div class="row d-flex justify-content-around ">-->
+            <!--        <div class="row justify-content-around justify-content-lg-center flex-md-row flex-sm-column">-->
+            <a class="navbar-brand bg-image" href="template.php">
+                <!--            <a class="navbar-brand bg-image order-md-1 order-lg-1" href="index.php">-->
+                <span>familienbande24.de</span>
+            </a>
+            <div class="ads d-none d-lg-block">
+                <!--            <div class="d-none d-xl-block order-md-1 order-lg-2">-->
+                <img class="leaderboard" src="https://dummyimage.com/970x90/eee/00ffd5.png." alt="Dummy Leaderboard">
+            </div>
+            <!-- align-self-center d-block d-md-none-->
+            <button class="navbar-toggler d-block d-md-none" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fas fa-bars"></i>
             </button>
-            <button class="search btn btn-outline-success align-self-center d-none d-xl-block order-lg-3" type="button" data-toggle="modal" data-target="#modalSearchDialog"><i class="fas fa-search"></i><span>Suche</span></button>
+            <button class="search btn btn-outline-success align-self-center order-md-2 order-lg-3 d-none d-xl-block" type="button" data-toggle="modal" data-target="#modalSearchDialog"><i class="fas fa-search"></i><span>Suche</span></button>
         </div>
     </div>
 </header>
