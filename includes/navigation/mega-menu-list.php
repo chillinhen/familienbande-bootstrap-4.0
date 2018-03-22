a0a9cbc91b70af466bd5ffcc054e4b0a79e12bb4
chillinhen <c.hillen@gmx.net>
2018-03-22 11:02:41 +0100

    mega-menu 2.0

---
+++ includes/navigation/mega-menu-list.php
@@ -0,0 +1,70 @@
+<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+    <div class="sub-cats">
+        <ul class="intro">
+            <li class="dropdown-header">Nette Kategorieüberschrift</li>
+            <li>Lorem ipsum dolor sit amet, consectetuer adipiscing 
+                elit. Aenean commodo ligula eget dolor. Aenean massa.</li>
+            <li>
+                <div id="small_carousel" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="https://loremflickr.com/150/100/paris,girl/all" alt="First slide">
+                            <div class="carousel-caption">
+                                <p class="small">Aenean commodo ligula</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="https://loremflickr.com/150/100/paris,girl/all" alt="Second slide">
+                            <div class="carousel-caption">
+                                <p class="small">separat existentie es un myth</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="https://loremflickr.com/150/100/paris,girl/all" alt="Third slide">
+                            <div class="carousel-caption">
+                                <p class="small">cientie, musica, sport etc Aenean commodo ligula eget dolor.Aenean commodo ligula eget dolor.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#small_carousel" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#small_carousel" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </li>
+            <li><a href="template-detail.php" class="btn btn-primary">Go somewhere</a></li>
+        </ul>
+<!--        <ul>
+            <li class="dropdown-header">Lorem ipsum dolor</li>
+            <li><a>Aenean commodo ligula eget dolor.</a></li>
+            <li><a>Aenean massa cum sociis natoque penatibus.</a></li>
+            <li><a>Li Europan lingues es membres del sam familie.</a></li>
+            <li class="dropdown-header">Consequat massa</li>
+            <li><a>Lor separat existentie es un myth.</a></li>
+            <li><a>Por scientie, musica, sport etc, litot Europa usa li sam vocabular.</a></li>
+            <li><a>Li lingues differe solmen in</a></li>
+        </ul>
+        <ul>
+            <li class="dropdown-header">Amet consectetuer</li>
+            <li><a>Aenean massa.</a></li>
+            <li><a>Cum sociis natoque penatibus</a></li>
+            <li><a>Nulla consequat massa quis enim.</a></li>
+            <li><a>Donec pede justo vulputate</a></li>
+            <li><a>In enim justo, rhoncus ut</a></li>
+            <li class="dropdown-header">Consequat massa</li>
+            <li><a>Lor separat existentie es un myth.</a></li>
+            <li><a>Por scientie, musica, sport etc, litot Europa usa li sam vocabular.</a></li>
+            <li><a>Li lingues differe solmen in</a></li>
+        </ul>-->
+    </div>
+</div>
+<!--<div class="d-flex flex-row justify-content-between align-start" aria-labelledby="navbarDropdown">
+
+
+
+
+</div>-->
