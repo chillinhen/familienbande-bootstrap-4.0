f90cb11c24c7167cb2fe302c4753c6f3264a8409
chillinhen <c.hillen@gmx.net>
2018-03-22 20:37:09 +0100

    mega-menu

--- includes/template/header.php
+++ includes/template/header.php
@@ -1,5 +1,5 @@
 <header id="header">
-    <div id="top-bar" class="col-12 mt-md-4 fixedsticky">
+    <div id="top-bar" class="col-12 mt-md-4">
         <div class="d-flex align-items-center justify-content-between container">
             <!--        <div class="row d-flex justify-content-around ">-->
             <!--        <div class="row justify-content-around justify-content-lg-center flex-md-row flex-sm-column">-->
